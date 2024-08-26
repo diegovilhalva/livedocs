322dd1b9032b85ea2b530b92049d979cd48ac43a
Diego Vilhalva <diegorocha450@yahoo.com>
2024-08-26 13:43:30 -0300

    added compoenents to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,29 +6,41 @@ import "./globals.css"
 import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-export const metadata:Metadata = {
-  title:'LiveDocs',
-  description:'Your go-to collaborative editor'
+export const metadata: Metadata = {
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor'
 }
 
-export default function RootLayout({ children }:{children:ReactNode}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+      variables:{colorPrimary:"#3371FF",
+        fontSize:"16px"
+      }
+    }}>
+      <html lang="en" suppressHydrationWarning>
 
-      <body
-        className={cn(
-          "min-h-screen  font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+        <body
+          className={cn(
+            "min-h-screen  font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+          {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
