5a4f7c62b67df8162ab98c8f41f69a609886aed0
Diego Vilhalva <diegorocha450@yahoo.com>
2024-08-27 09:39:15 -0300

    added livebllos room

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,34 +1,34 @@
 "use server"
 
-import {nanoid} from "nanoid"
+import { nanoid } from "nanoid"
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
 import { parseStringify } from "../utils";
 
-export const createDocument = async  ({userId,email}:CreateDocumentParams) => {
-    const roomId = nanoid() 
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
+    const roomId = nanoid();
+
     try {
         const metadata = {
-            creatorId:userId,
+            creatorId: userId,
             email,
-            title:"Untitled"
+            title: 'Untitled'
         }
 
-        const usersAccesses:RoomAccesses ={
-            [email]:['room:write']
-        } 
+        const usersAccesses: RoomAccesses = {
+            [email]: ['room:write']
+        }
+
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses:[]
-          
-          });
+            defaultAccesses: []
+        });
 
-          revalidatePath("/")
+        revalidatePath('/');
 
-          return parseStringify(room)
-        
+        return parseStringify(room);
     } catch (error) {
-        console.log(`Erro happened while creating a room`)
+        console.log(`Error happened while creating a room: ${error}`);
     }
 }
