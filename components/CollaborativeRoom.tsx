3061aaf21261fd113b4eea0cf78caf8c8f2e4fdd
Diego Vilhalva <diegorocha450@yahoo.com>
2024-08-28 13:15:31 -0300

    sentry setup

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,8 +11,8 @@ import Image from "next/image"
 import { updateDocument } from "@/lib/actions/room.actions"
 
 
-const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
-    const currentUserType = "editor"
+const CollaborativeRoom = ({ roomId, roomMetadata,users,currentUserType }: CollaborativeRoomProps) => {
+  
     const [editing, setEditing] = useState(false)
     const [loading, setLoading] = useState(false)
     const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
@@ -88,7 +88,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
                             </SignedIn>
                         </div>
                     </Header>
-                    <Editor />
+                    <Editor roomId={roomId} currentUserType={currentUserType} />
                 </div>
             </ClientSideSuspense>
         </RoomProvider>
